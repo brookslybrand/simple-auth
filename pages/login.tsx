c44f333f9481d2e8d6a69e17a8d709f88aa236bd
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-05-10 07:57:40 -0500

    Add tailwindcss

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,17 +8,19 @@ export default function Login() {
       <Header>
         <title>Login</title>
       </Header>
-      <main>
-        <div>
-          <h1>Log in</h1>
-          <button
-            onClick={() => {
-              signIn("google", { callbackUrl });
-            }}
-          >
-            Continue with google
-          </button>
-        </div>
+      <main className="flex flex-col max-w-max mx-auto mt-8 space-y-8 items-center">
+        <h1 className="text-5xl">Log in</h1>
+        <button
+          className="
+            rounded-md p-2 text-lg text-gray-900 border-2 border-green-600
+            hover:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-green-600
+          "
+          onClick={() => {
+            signIn("google", { callbackUrl });
+          }}
+        >
+          Continue with google
+        </button>
       </main>
     </>
   );
