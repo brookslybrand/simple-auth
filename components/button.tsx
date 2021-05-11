ebb448cac6e9f928ac8d5fb185017d8897451de3
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-05-11 07:59:42 -0500

    Add spinner

---
+++ components/button.tsx
@@ -0,0 +1,21 @@
+import { forwardRef } from "react";
+import clsx from "clsx";
+
+const Button = forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithRef<"button">
+>(function Button({ className, ...props }, ref) {
+  return (
+    <button
+      ref={ref}
+      className={clsx([
+        `rounded-md p-2 text-lg text-gray-900 border-2 border-green-600
+         hover:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-green-600`,
+        className,
+      ])}
+      {...props}
+    />
+  );
+});
+
+export default Button;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,8 @@
 import Header from "next/head";
 import { signIn } from "next-auth/client";
 
+import Button from "../components/button";
+
 const callbackUrl = `/`;
 export default function Login() {
   return (
@@ -10,17 +12,13 @@ export default function Login() {
       </Header>
       <main className="flex flex-col max-w-max mx-auto mt-8 space-y-8 items-center">
         <h1 className="text-5xl">Log in</h1>
-        <button
-          className="
-            rounded-md p-2 text-lg text-gray-900 border-2 border-green-600
-            hover:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-green-600
-          "
+        <Button
           onClick={() => {
             signIn("google", { callbackUrl });
           }}
         >
           Continue with google
-        </button>
+        </Button>
       </main>
     </>
   );
