4675113dc51e5b04d5481efa4dfd3c6a6a0d29a5
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-05-18 08:34:04 -0500

    Connect email provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,5 +7,17 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
+    Providers.Email({
+      server: process.env.EMAIL_SERVER ?? "",
+      from: process.env.EMAIL_FROM,
+    }),
   ],
+  database: {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    // this is fine since this is a very simple application
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
